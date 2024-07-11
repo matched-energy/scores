2b1dac201c1e09e5a8cda4c6122b0b9a8472060a
Joe Kwiatkowski <joejk2@gmail.com>
2024-07-11 14:18:21 +0100

    Top-level switch between 'to_review' & 'overwrite'

--- workflow/supplier_scores.py
+++ workflow/supplier_scores.py
@@ -8,11 +8,15 @@ import scores.supplier_time_matched_scores
 from scores.configuration import conf
 
 
-def setup(paths, overwrite=False):
-    if not overwrite:
-        os.mkdir(paths["OUTPUT"]["processed_to_review"])
-        os.mkdir(os.path.join(paths["OUTPUT"]["processed_to_review"], "S0142"))
-        os.mkdir(paths["OUTPUT"]["final_to_review"])
+def setup(paths):
+    dirs = [
+        paths["OUTPUT"]["processed"],
+        os.path.join(paths["OUTPUT"]["processed"], "S0142"),
+        paths["OUTPUT"]["final"],
+    ]
+    for dir in dirs:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
 
 
 def extract_regos(paths, supplier):
@@ -32,7 +36,7 @@ def calculate_scores(paths, supplier):
         path_grid_month_tech=paths["GRID"]["volumes_by_tech_by_month"],
         path_grid_hh_generation=paths["GRID"]["volumes_by_tech_by_half_hour"],
         path_supplier_hh_load=os.path.join(
-            paths["OUTPUT"]["final_to_review"],
+            paths["OUTPUT"]["final"],
             f"{supplier['bsc_lead_party_id']}_load.csv",
         ),
     )
@@ -45,7 +49,7 @@ def process_s0142_files(paths):
             "S0142_20220401_SF_20220427115520.gz",
             "S0142_20220402_SF_20220427114051.gz",
         ],
-        output_dir=os.path.join(paths["OUTPUT"]["processed_to_review"], "S0142"),
+        output_dir=os.path.join(paths["OUTPUT"]["processed"], "S0142"),
         bsc_party_ids=["PURE", "MERCURY"],
     )
 
@@ -53,9 +57,9 @@ def process_s0142_files(paths):
 def aggregate_supplier_load(paths, supplier):
     scores.s0142.concatenate_days.main(
         bsc_lead_party_id=supplier["bsc_lead_party_id"],
-        input_dir=os.path.join(paths["OUTPUT"]["processed_to_review"], "S0142"),
+        input_dir=os.path.join(paths["OUTPUT"]["processed"], "S0142"),
         output_path=os.path.join(
-            paths["OUTPUT"]["final_to_review"],
+            paths["OUTPUT"]["final"],
             f"{supplier['bsc_lead_party_id']}_load.csv",
         ),
     )
@@ -68,13 +72,20 @@ def process_supplier(paths, supplier):
     return results
 
 
-def process_suppliers(suppliers="all"):
+def process_suppliers(suppliers="all", overwrite=False):
     ## Where does year belong? Needs apply to regos and grid.
     ## ^   should handle in pre-processing
     ## ... and be explicit about timeranges in filename
     ## ... and have functions that validate data range & completeness
     paths = conf.read("paths.yaml")
+    if overwrite:
+        paths["OUTPUT"]["processed"] = paths["OUTPUT"]["processed_overwrite"]
+        paths["OUTPUT"]["final"] = paths["OUTPUT"]["final_overwrite"]
+    else:
+        paths["OUTPUT"]["processed"] = paths["OUTPUT"]["processed_to_review"]
+        paths["OUTPUT"]["final"] = paths["OUTPUT"]["final_to_review"]
     setup(paths)
+
     process_s0142_files(paths)
 
     results = {}
