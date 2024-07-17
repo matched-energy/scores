4bdb0825f4ce601c7abc158936e0742be4268e20
Joe Kwiatkowski <joejk2@gmail.com>
2024-07-17 14:11:02 +0100

    Breakout calculation of hh_rego_total

--- core/supplier_scores.py
+++ core/supplier_scores.py
@@ -7,21 +7,26 @@ import scores.configuration.conf as conf
 import scores.plot.plot_supplier
 
 
+def t_hh_rego_total(hh_rego):
+    tech_categories = conf.read("generation.yaml", conf_dir=True)["TECH"]  # TODO
+    return hh_rego[[f"{tech}_supplier" for tech in tech_categories]].sum(axis=1)
+
+
+def calc_scores_new(hh_generation, hh_load):
+    pass
+
+
 def calc_scores(hh_generation, hh_load):
     ## TODO - assert timeseries are aligned
-    TECH = conf.read("generation.yaml", conf_dir=True)["TECH"]  # TODO
-    hh_generation["TOTAL"] = hh_generation[[f"{tech}_supplier" for tech in TECH]].sum(
-        axis=1
-    )
+    hh_rego_total = t_hh_rego_total(hh_generation)
     load = -hh_load["BM Unit Metered Volume"]
-    hh_load["deficit"] = (load - hh_generation["TOTAL"]).clip(lower=0)
+    hh_load["deficit"] = (load - hh_rego_total).clip(lower=0)
     scores = collections.OrderedDict(
-        generation_total=hh_generation["TOTAL"].sum(),
+        generation_total=hh_rego_total.sum(),
         load_total=load.sum(),
         unmatched_volume_hh=hh_load["deficit"].sum(),
         matched_volume_percent_hh=(1 - hh_load["deficit"].sum() / (load.sum())) * 100,
-        matched_volume_percent_annual=(hh_generation["TOTAL"].sum() / (load.sum()))
-        * 100,
+        matched_volume_percent_annual=(hh_rego_total.sum() / (load.sum())) * 100,
     )
     pprint.pprint(scores)
     return scores
@@ -44,6 +49,7 @@ def main(
     if plot:
         scores.plot.plot_supplier.plot_and_gen(hh_generation, hh_load)
 
+    calc_scores_new(hh_generation, hh_load)
     return calc_scores(hh_generation, hh_load)
 
 
