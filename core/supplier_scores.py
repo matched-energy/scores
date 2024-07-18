5fc3818a92cae2667efab40cd1c4e04a96036962
Joe Kwiatkowski <joejk2@gmail.com>
2024-07-18 13:23:53 +0100

    Tidy suppliers scores

--- core/supplier_scores.py
+++ core/supplier_scores.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 import pandas as pd
 import scipy.optimize
+
 import scores.configuration.conf as conf
 
 
@@ -13,29 +14,34 @@ def t_hh_rego_total(hh_rego):
     return hh_rego[[f"{tech}_supplier" for tech in tech_categories]].sum(axis=1)
 
 
-def assemble_stats(bsc_hh, r_hh, g_hh, l_hh):
+def assemble_stats(bsc_hh, r_hh, g_hh, l_hh, embedded_ratio):
     l_hh_sum = l_hh.sum()
     g_hh_sum = g_hh.sum()
 
-    # r_yr_sum = (l_hh.sum() - g_hh.sum()).clip(min=0)
-    # r_yr_sum = (-bsc_vol).sum().clip(min=0)
-    r_yr_sum = (r_hh.sum()).clip(min=0)
-    # r_hh_sum = (l_hh - g_hh).clip(lower=0).sum()
-    # r_hh_sum = (-bsc_vol).clip(lower=0).sum()
+    r_yr = (r_hh.sum()).clip(min=0)
     r_hh_sum = (r_hh).clip(lower=0).sum()
 
-    s_yr = 1 - r_yr_sum / l_hh_sum
+    l_metered_discernable_yr = -bsc_hh["BM Unit Metered Volume: -ve"].sum()
+    g_embedded_metered_discernable_yr = bsc_hh["BM Unit Metered Volume: +ve"].sum()
+
+    s_yr = 1 - r_yr / l_hh_sum
     s_hh = 1 - r_hh_sum / l_hh_sum
 
     return collections.OrderedDict(
         bsc_hh_sum=bsc_hh["BM Unit Metered Volume"].sum(),
         g_hh_sum=g_hh_sum,
+        g_embedded_metered_discernable_yr=g_embedded_metered_discernable_yr,
+        g_embedded_metered_discernable_ratio=g_embedded_metered_discernable_yr
+        / g_hh_sum,
         l_hh_sum=l_hh_sum,
-        r_yr_sum=r_yr_sum,
+        l_metered_discernable_hh=l_metered_discernable_yr,
+        l_metered_discernable_ratio=l_metered_discernable_yr / l_hh_sum,
+        r_yr=r_yr,
         r_hh_sum=r_hh_sum,
         s_yr=s_yr,
         s_hh=s_hh,
         g_l_ratio=g_hh_sum / l_hh_sum,
+        embedded=embedded_ratio,
     )
 
 
@@ -43,7 +49,7 @@ def independent(g_rego_hh, bsc_hh):
     g_hh = g_rego_hh
     l_hh = -bsc_hh["BM Unit Metered Volume"]
     r_hh = l_hh - g_hh
-    return assemble_stats(bsc_hh, r_hh, g_hh, l_hh)
+    return assemble_stats(bsc_hh, r_hh, g_hh, l_hh, embedded_ratio=0)
 
 
 def embedded(g_rego_hh, bsc_hh):
@@ -51,28 +57,25 @@ def embedded(g_rego_hh, bsc_hh):
     l_hh = g_rego_hh - bsc_hh["BM Unit Metered Volume"]
     r_hh = -bsc_hh["BM Unit Metered Volume"]
 
-    return assemble_stats(bsc_hh, r_hh, g_hh, l_hh)
+    return assemble_stats(bsc_hh, r_hh, g_hh, l_hh, embedded_ratio=1)
 
 
-def approach_4(g_rego_hh, bsc_hh):
+def opt(g_rego_hh, bsc_hh):
 
-    def f(r):
-        g_embedded_hh = r * g_rego_hh
+    def f(x):
+        g_embedded_hh = x * g_rego_hh
         l_hh = g_embedded_hh - bsc_hh["BM Unit Metered Volume"]
         return abs(l_hh.sum() - g_rego_hh.sum())
 
-    result = scipy.optimize.minimize_scalar(f, bounds=(0, 1), method="bounded")
-    r = result.x
+    opt = scipy.optimize.minimize_scalar(f, bounds=(0, 1), method="bounded")
 
     g_hh = g_rego_hh
-    g_embedded_hh = r * g_rego_hh
+    g_embedded_hh = opt.x * g_rego_hh
     l_hh = g_embedded_hh - bsc_hh["BM Unit Metered Volume"]
-    g_independent_hh = (1 - r) * g_rego_hh
+    g_independent_hh = (1 - opt.x) * g_rego_hh
     r_hh = -bsc_hh["BM Unit Metered Volume"] - g_independent_hh
 
-    results = assemble_stats(bsc_hh, r_hh, g_hh, l_hh)
-    results.update({"r": r}, inplace=True)
-    return results
+    return assemble_stats(bsc_hh, r_hh, g_hh, l_hh, embedded_ratio=opt.x)
 
 
 def get_supplier_load(path_supplier_hh_load):
@@ -99,7 +102,7 @@ def main(
     scores = {}
     scores["independent"] = independent(g_rego_hh, hh_load)
     scores["embedded"] = embedded(g_rego_hh, hh_load)
-    scores["approach_4"] = approach_4(g_rego_hh, hh_load)
+    scores["opt"] = opt(g_rego_hh, hh_load)
     return scores
 
 
