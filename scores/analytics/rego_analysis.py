2eefe5e831002aef01270887888dd7badca8bb7b
Joe Kwiatkowski <joejk2@gmail.com>
2024-10-07 13:05:53 +0100

    Tidier labeling axis labeling

--- scores/analytics/rego_analysis.py
+++ scores/analytics/rego_analysis.py
@@ -16,13 +16,13 @@ def format_number(num):
     if num < 1000:
         return str(num)
     elif 1000 <= num < 1e6:
-        return f"{num // 1000}k"
+        return f"{num / 1000:.1f}k"
     elif 1e6 <= num < 1e9:
-        return f"{num // 1e6}M"
+        return f"{num / 1e6:.1f}M"
     elif 1e9 <= num < 1e12:
-        return f"{num // 1e9}B"
+        return f"{num / 1e9:.1f}B"
     else:
-        return f"{num // 1e12}T"
+        return f"{num / 1e12:.1f}T"
 
 
 tech_simple_colors = dict(
@@ -136,13 +136,11 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         fig.write_html(output_path)
         return
 
-    ## TOP-LEFT
+    ## PARETO
     fig.add_trace(
         go.Bar(
             x=regos_supplier_by_station.index,
             y=regos_supplier_by_station["MWh"],
-            xaxis="x1",
-            yaxis="y1",
             marker=dict(color=regos_supplier_by_station_tech_simple_color_map),
             showlegend=False,
         ),
@@ -168,8 +166,6 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         go.Scatter(
             x=regos_supplier_by_station_MWh_cumsum.index,
             y=regos_supplier_by_station_MWh_cumsum.values,
-            xaxis="x1",
-            yaxis="y2",
             showlegend=False,
         ),
         col=1,
@@ -191,8 +187,50 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         font=dict(size=8),
         secondary_y=True,
     )
+    fig.update_xaxes(tickangle=-45, matches="x4", showticklabels=False, col=1, row=1)
+    fig.update_yaxes(
+        title=dict(text="<b>MWh</b>", font=dict(size=8)),
+        type="log",
+        dtick=1,
+        col=1,
+        row=1,
+        secondary_y=False,
+    )
+    fig.update_yaxes(
+        showticklabels=False,
+        overlaying="y",
+        side="right",
+        col=1,
+        row=1,
+        secondary_y=True,
+    )
 
-    ## TOP-RIGHT
+    ## FRACTIONAL OFFTAKE
+    fractional_offtake = (
+        100 * regos_supplier_by_station["MWh"] / regos_all_by_station.values
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=regos_supplier_by_station.index,
+            y=fractional_offtake,
+            showlegend=False,
+        ),
+        col=1,
+        row=3,
+    )
+    fig.update_xaxes(
+        title=dict(font=dict(size=8)),
+        tickangle=-45,
+        col=1,
+        row=3,
+    )
+    fig.update_yaxes(
+        title=dict(text="<b>%</b>", font=dict(size=8)),
+        dtick=25,
+        col=1,
+        row=3,
+    )
+    ## OFFTAKE VOLUMES
     x2 = [
         f"> {format_number(interval.left)} MWh"
         for interval in regos_supplier_by_station_hist.index
@@ -201,49 +239,37 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         go.Bar(
             x=x2,
             y=regos_supplier_by_station_hist.values,
-            xaxis="x2",
-            yaxis="y3",
             showlegend=False,
         ),
         col=2,
         row=1,
         secondary_y=False,
     )
-    # fig.add_trace(
-    #     go.Scatter(
-    #         x=x2,
-    #         y=regos_supplier_by_station_hist.values.cumsum(),
-    #         xaxis="x2",
-    #         yaxis="y4",
-    #         showlegend=False,
-    #     ),
-    #     col=2,
-    #     row=1,
-    #     secondary_y=True,
-    # )
-
-    ## BOTTOM-LEFT
-    fractional_offtake = (
-        100 * regos_supplier_by_station["MWh"] / regos_all_by_station.values
+    fig.update_xaxes(tickangle=-45, col=2, row=1)
+    fig.update_yaxes(
+        title=dict(text="<b>Generator count</b>", font=dict(size=8)),
+        col=2,
+        row=1,
     )
+
+    ## BY TECH
     fig.add_trace(
-        go.Scatter(
-            x=regos_supplier_by_station.index,
-            y=fractional_offtake,
-            xaxis="x4",
-            yaxis="y7",
+        go.Bar(
+            x=regos_supplier_by_tech_simple.index,
+            y=regos_supplier_by_tech_simple.values,
             showlegend=False,
         ),
-        col=1,
-        row=3,
+        col=2,
+        row=2,
     )
+
+    ## BY OWNERSHIP
     shared = regos_supplier_by_station[fractional_offtake - 100 < 0]
     exclusive = regos_supplier_by_station[fractional_offtake - 100 == 0]
     fig.add_trace(
         go.Bar(
             x=["shared", "exclusive"],
             y=[shared["MWh"].sum(), exclusive["MWh"].sum()],
-            yaxis="y9",
             showlegend=False,
         ),
         col=2,
@@ -266,7 +292,6 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         go.Scatter(
             x=["shared", "exclusive"],
             y=shared_generator_count,
-            yaxis="y9",
             mode="markers",
             showlegend=False,
         ),
@@ -286,41 +311,20 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
         font=dict(size=8),
         secondary_y=True,
     )
-
-    ## BOTTOM-RIGHT
-    fig.add_trace(
-        go.Bar(
-            x=regos_supplier_by_tech_simple.index,
-            y=regos_supplier_by_tech_simple.values,
-            xaxis="x3",
-            yaxis="y5",
-            showlegend=False,
-        ),
+    fig.update_yaxes(
+        title=dict(text="<b>MWh</b>", font=dict(size=8)),
         col=2,
-        row=2,
+        row=3,
+    )
+    fig.update_yaxes(
+        range=[0, math.ceil(shared_generator_count[0] / 50) * 50],
+        title=dict(text="<b>Count</b>", font=dict(size=8)),
+        col=2,
+        row=3,
+        secondary_y=True,
     )
 
     fig.update_layout(
-        xaxis=dict(tickangle=-45, matches="x4", showticklabels=False),
-        yaxis=dict(
-            title=dict(text="<b>MWh</b>", font=dict(size=8)),
-            type="log",
-            dtick=1,
-        ),
-        yaxis2=dict(showticklabels=False, overlaying="y", side="right"),
-        # yaxis4=dict(title="Cumulative number of generators", dtick=250),
-        xaxis2=dict(tickangle=-45),
-        xaxis4=dict(tickangle=-45, tickfont=dict(size=8)),
-        yaxis3=dict(
-            title=dict(text="<b>Generator count</b>", font=dict(size=8)),
-        ),
-        yaxis5=dict(title=dict(text="<b>MWh</b>", font=dict(size=8))),
-        yaxis7=dict(title=dict(text="<b>%</b>", font=dict(size=8)), dtick=25),
-        yaxis9=dict(title=dict(text="<b>MWh</b>", font=dict(size=8))),
-        yaxis10=dict(
-            range=[0, math.ceil(shared_generator_count[0] / 50) * 50],
-            title=dict(text="<b>Count</b>", font=dict(size=8)),
-        ),
         annotations=[
             (
                 dict(
@@ -340,13 +344,13 @@ def plot_supplier(regos: Path, current_holder: str, output_path: Path) -> None:
             for annotation in fig["layout"]["annotations"]
         ],
         legend=dict(
-            orientation="h",  # Horizontal legend
+            orientation="h",
             x=0.67,
             y=0.38,
-            xanchor="right",  # Anchor the legend's x position to the right
-            yanchor="top",  # Anchor the legend's y position to the top
-            font=dict(size=8),  # Reduce font size
-            tracegroupgap=2,  # Reduce space between items
+            xanchor="right",
+            yanchor="top",
+            font=dict(size=8),
+            tracegroupgap=2,
         ),
         plot_bgcolor="rgba(255,255,255,1)",
         paper_bgcolor="rgba(255,255,255,1)",
